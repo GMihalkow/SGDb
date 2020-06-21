750c8323048892a1bce29eccc0d6c658bdf9e335
Georgi <georgimihalkov@abv.bg>
2020-06-21 15:58:58 +0300

    Created ValidationSummary and ValidationMessages components...
    Created ValidationSummary and ValidationMessages components to help with form validation with Vuelidate.

---
+++ sgdb.client/src/components/partials/ValidationMessages.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div v-if="validationContext.$dirty">
+            <div v-bind:key="param.id" v-for="param in validationContext.$params">
+                <div class="error" v-if="!validationContext[param.type]">{{validationMessage(param, propName)}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ValidationMessages',
+        props: ['validationContext', 'propName'],
+        methods: {
+            validationMessage: function (prop, propName) {
+                const validationMessages = {
+                    required: '{0} is a required field.'.replace('{0}', propName),
+                    minLength: '{0} must be at least {1} characters long.'.replace('{0}', propName).replace('{1}', prop.min),
+                    maxLength: '{0} must be maximum {1} characters long.'.replace('{0}', propName).replace('{1}', prop.max),
+                    email: '{0} must be a valid email format.'.replace('{0}', propName),
+                    numeric: '{0} must contain only numeric values.'.replace('{0}', propName),
+                    sameAs: '{0} must be the same as {1}.'.replace('{0}', propName).replace('{1}', prop.eq)
+                };
+
+                return validationMessages[prop.type];
+            }
+        }
+    }
+</script>
+
+<!-- TODO [GM]: remove? -->
+<style scoped>
+
+</style>
---
+++ sgdb.client/src/components/partials/ValidationSummary.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="errors.length > 0" class="p-field p-col-12 p-md-8 p-sm-12 text-center">
+        <ul class="validation-summary" v-bind:key="error.id" v-for="error in errors">
+            <li class="error">{{error}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ValidationSummary',
+        props: ['errors']
+    }
+</script>
+
+<style scoped>
+    .validation-summary {
+        list-style: none;
+        text-decoration: none;
+    }
+</style>
