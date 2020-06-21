a354a8b8b35dc9853696d3e13478570fd5d1da50
Georgi <georgimihalkov@abv.bg>
2020-06-21 15:59:28 +0300

    Styling changes and added the footer to the App component.

--- sgdb.client/src/App.vue
+++ sgdb.client/src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <div id="app" class="main">
+  <div id="app" class="main-container">
     <Nav/>
-    <router-view/>
+    <div class="main">
+      <router-view/>
+    </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import Nav from './components/layout/Nav.vue';
+  import Nav from './components/layout/Nav';
+  import Footer from './components/layout/Footer';
 
-export default {
-  name: 'App',
-  components: {
-    Nav: Nav
+  export default {
+    name: 'App',
+    components: {
+      Nav: Nav,
+      Footer: Footer
+    }
   }
-}
 </script>
 
 <style>
   .main {
+    max-height: 100%;
+    max-width: 60rem;
+    margin: 0 auto;
+  }
+
+  .main-container {
     height: 100%;
   }
 </style>
