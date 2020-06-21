58fd3fd66e324df3288f34dc3abec57601181684
Georgi <georgimihalkov@abv.bg>
2020-06-21 15:58:23 +0300

    Created a footer partial component.

---
+++ sgdb.client/src/components/layout/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer main-bg-dark">
+        All Rights Reserved &copy; 2020 SoftUni - Microservice Architectures
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'        
+    }
+</script>
+
+<style scoped>
+    .footer {
+        position: absolute;
+        display: block;
+        text-align: center;
+        color: #fff;
+        padding: 0.5rem;
+        width: 100%;
+        font-weight: bold;
+        bottom: 0;
+        left: 0;
+        font-size: 14px;
+    }
+</style>
