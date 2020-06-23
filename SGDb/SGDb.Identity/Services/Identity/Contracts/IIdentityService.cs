b1a951e71c2c3e739afb9bec54d30d27ddd54e68
Georgi <georgimihalkov@abv.bg>
2020-06-23 16:08:58 +0300

    Commited the Register and Login IdentityService implementations.

--- SGDb/SGDb.Identity/Services/Identity/Contracts/IIdentityService.cs
+++ SGDb/SGDb.Identity/Services/Identity/Contracts/IIdentityService.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+using SGDb.Identity.Models.Identity;
+
 namespace SGDb.Identity.Services.Identity.Contracts
 {
     public interface IIdentityService
     {
-        
+        Task Register(RegisterInputModel registerInputModel);
+
+        Task<bool> Login(LoginInputModel loginInputModel);
     }
 }
--- SGDb/SGDb.Identity/Services/Identity/IdentityService.cs
+++ SGDb/SGDb.Identity/Services/Identity/IdentityService.cs
@@ -1,9 +1,41 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SGDb.Identity.Data.Models;
+using SGDb.Identity.Models.Identity;
 using SGDb.Identity.Services.Identity.Contracts;
 
 namespace SGDb.Identity.Services.Identity
 {
     public class IdentityService : IIdentityService
     {
-        
+        private readonly SignInManager<User> _signInManager;
+
+        public IdentityService(SignInManager<User> signInManager)
+        {
+            this._signInManager = signInManager;
+        }
+
+        public async Task Register(RegisterInputModel registerInputModel)
+        {
+            var user = new User
+            {
+                UserName = registerInputModel.Username,
+                Email = registerInputModel.EmailAddress,
+                PhoneNumber = registerInputModel.PhoneNumber
+            };
+
+            var identityResult = await this._signInManager.UserManager.CreateAsync(user, registerInputModel.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                throw new InvalidOperationException(identityResult.Errors.FirstOrDefault()?.Description);
+            }
+        }
+
+        public async Task<bool> Login(LoginInputModel loginInputModel) =>
+            (await this._signInManager.PasswordSignInAsync(loginInputModel.Username, loginInputModel.Password, true,
+                false)).Succeeded;
     }
 }
