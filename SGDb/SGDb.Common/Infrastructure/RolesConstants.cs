45738d9c08713a375c4f55e76c0902bcf7d48fce
Georgi <georgimihalkov@abv.bg>
2020-07-05 16:34:36 +0300

    Added Roles and Web constants.

---
+++ SGDb/SGDb.Common/Infrastructure/RolesConstants.cs
@@ -0,0 +1,11 @@
+namespace SGDb.Common.Infrastructure
+{
+    public class RolesConstants
+    {
+        public const string Administrator = "Administrator";
+
+        public const string Creator = "Creator";
+
+        public const string User = "User";
+    }
+}
---
+++ SGDb/SGDb.Common/Infrastructure/WebConstants.cs
@@ -0,0 +1,9 @@
+namespace SGDb.Common.Infrastructure
+{
+    public class WebConstants
+    {
+        public const string AuthenticationCookieName = "Authentication";
+        public const string AuthorizationHeaderName = "Authorization";
+        public const string AuthorizationHeaderValuePrefix = "Bearer";
+    }
+}
