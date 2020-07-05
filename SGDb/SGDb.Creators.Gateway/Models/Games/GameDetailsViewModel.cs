5c7231a38e26f9c2e383dc8f3222743c0a64ae6e
Georgi <georgimihalkov@abv.bg>
2020-07-05 16:50:43 +0300

    Added the GameDetailsViewModel.

---
+++ SGDb/SGDb.Creators.Gateway/Models/Games/GameDetailsViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SGDb.Creators.Gateway.Models.Games
+{
+    public class GameDetailsViewModel
+    {
+        public string Name { get; set; }
+
+        public string HeaderImageUrl { get; set; }
+
+        public string BackgroundImageUrl { get; set; }
+
+        public string WebsiteUrl { get; set; }
+
+        public string About { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public uint? Recommendations { get; set; }
+
+        public DateTime? ReleasedOn { get; set; }
+
+        public uint Views { get; set; }
+    }
+}
