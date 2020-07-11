22e4221c9cebd8f22143fbbf278f09efdedf19e4
GMihalkow <georgimihalkov@abv.bg>
2020-07-11 11:02:09 +0300

    Created an AuthorizeMultipleRoles convenience attribute.

---
+++ SGDb/SGDb.Common/Infrastructure/Attributes/Authorization/AuthorizeMultipleRolesAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace SGDb.Common.Infrastructure.Attributes.Authorization
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+    public class AuthorizeMultipleRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeMultipleRolesAttribute(string[] roles) => this.Roles = string.Join(", ", roles);
+    }
+}
