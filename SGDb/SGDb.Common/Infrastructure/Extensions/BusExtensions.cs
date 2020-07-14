52dd05f79c83597fc69d1ac0b9f58bd62c2a32c6
GMihalkow <georgimihalkov@abv.bg>
2020-07-14 18:45:59 +0300

    Code clean up and refactoring.

--- SGDb/SGDb.Common/Infrastructure/Extensions/BusExtensions.cs
+++ SGDb/SGDb.Common/Infrastructure/Extensions/BusExtensions.cs
@@ -8,7 +8,7 @@ namespace SGDb.Common.Infrastructure.Extensions
     public static class BusExtensions
     {
         public static async Task PublishWithTimeout<TMessage>(this IBus bus, TMessage message)
-            => await bus.Publish(message, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+            => await bus.PublishWithTimeout(message, message.GetType());
         
         public static async Task PublishWithTimeout<TMessage>(this IBus bus, TMessage message, Type type)
             => await bus.Publish(message, type, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
