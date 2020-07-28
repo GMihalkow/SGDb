a74af56cfa011b9e75fe5092374b5ec5b2e94e96
GMihalkov <georgimihalkov@abv.bg>
2020-07-28 18:42:32 +0300

    Added extra validation in the ChangePasswordInputModel.

--- SGDb/SGDb.Identity/Models/Identity/ChangePasswordInputModel.cs
+++ SGDb/SGDb.Identity/Models/Identity/ChangePasswordInputModel.cs
@@ -1,14 +1,18 @@
-using System.ComponentModel.DataAnnotations;
 using SGDb.Common.Infrastructure.Attributes.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace SGDb.Identity.Models.Identity
 {
     public class ChangePasswordInputModel
     {
         [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
         
         [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
         [NotEqualTo(nameof(CurrentPassword))]
         public string NewPassword { get; set; }
     }
