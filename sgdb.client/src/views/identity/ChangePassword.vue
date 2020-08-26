2c38619f1e08fc19c0c58a0a4469b8efc2b9a8c7
GMihalkov <georgimihalkov@abv.bg>
2020-08-26 21:58:34 +0300

    Added frontend validation to the ChangePassword component's input fields.

--- sgdb.client/src/views/identity/ChangePassword.vue
+++ sgdb.client/src/views/identity/ChangePassword.vue
@@ -54,7 +54,7 @@
     import ValidationSummary from '../../components/partials/ValidationSummary';
     import ValidationMessages from '../../components/partials/ValidationMessages';
 
-    import { required, sameAs, not } from 'vuelidate/lib/validators';
+    import { minLength, required, sameAs, not } from 'vuelidate/lib/validators';
     import identityApi from '../../api/identity/identity-api';
 
     export default {
@@ -78,11 +78,13 @@
         validations: {
             form: {
                 currentPassword: {
-                    required: required
+                    required: required,
+                    minLength: minLength(6)
                 },
                 newPassword: {
                     not: not(sameAs('currentPassword')),
-                    required: required
+                    required: required,
+                    minLength: minLength(6)
                 }
             }
         },
