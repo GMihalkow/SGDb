1a6bc7a1b255ffe6cc373a9c3aada96808e527c0
GMihalkov <georgimihalkov@abv.bg>
2020-08-26 21:54:48 +0300

    Added frontend validation to the ChangePassword component's input fields.

---
+++ SGDb/SGDb.Identity/Models/Roles/RoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SGDb.Identity.Models.Roles
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ SGDb/SGDb.Identity/Services/UserRoles/UserRolesService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGDb.Common.Infrastructure.Extensions;
+using SGDb.Identity.Data;
+using SGDb.Identity.Models.UserRoles;
+using SGDb.Identity.Services.Roles.Contracts;
+using SGDb.Identity.Services.UserRoles.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGDb.Identity.Services.UserRoles
+{
+    public class UserRolesService : IUserRolesService
+    {
+        private readonly IdentityDbContext _dbContext;
+        private readonly IRolesService _rolesService;
+
+        public UserRolesService(IdentityDbContext dbContext, IRolesService rolesService)
+        {
+            this._dbContext = dbContext;
+            this._rolesService = rolesService;
+        }
+
+        public async Task<IEnumerable<UserRoleViewModel>> GetAllByUserIds(string[] userIds = null)
+        {
+            var userRoles = await this._dbContext.UserRoles
+                .Where(ur => userIds.IsNullOrEmpty() || userIds.Contains(ur.UserId))
+                .Select(ur => new UserRoleViewModel
+                {
+                    RoleId = ur.RoleId,
+                    UserId = ur.UserId
+                })
+                .ToListAsync();
+
+            var roles = await this._rolesService.GetAll(userRoles?.Select(ur => ur.RoleId)?.ToArray());
+
+            roles.ForEach((r) =>
+            {
+                var userRole = userRoles.FirstOrDefault(ur => ur.RoleId == r.Id);
+                
+                if (userRole != null)
+                {
+                    userRole.RoleName = r.Name;
+                }
+            });
+
+            return default;
+        }
+    }
+}
